5cbba3b610696ed5e1733ada7a64780fff40f2d4
Abadima <abadima06@gmail.com>
2022-12-22 17:13:17 -0500

    v2.0.2
    UPDATES:
    
    - Package Updates
    - Simply-XP Beta
    - Update index

--- index.js
+++ index.js
@@ -17,13 +17,19 @@ const client = new Client({
 }); 
 
 client.on("ready", async () => {
-	const wok = new Wok(client, {
+	console.log(`BOT ACTIVE: ${client.user.tag}`);
+
+	new Wok({
+
+		client,
 
 		commandsDir: path.join(__dirname, "commands"),
 
-		featuresDir: path.join(__dirname, "features"),
-
-		showWarns: true,
+		cooldownConfig: {
+			errorMessage: "Please wait {TIME} before doing that again.",
+			botOwnersBypass: true,
+			dbRequired: 60,
+		},
 
 		delErrMsgCooldown: 5,
 
@@ -32,23 +38,21 @@ client.on("ready", async () => {
 		dbOptions: {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
+			strictQuery: false,
 			keepAlive: true,
 		},
 
 		disabledDefaultCommands: [
-			"help",
-			"prefix",
-			"slash",
-			"command",
-			"language",
-			"requiredrole",
-			"channelonly"
-		], mongoUri: process.env.DB,
-	}).setDefaultPrefix("​")
+			Wok.DefaultCommands.ChannelCommand,
+			Wok.DefaultCommands.CustomCommand,
+			Wok.DefaultCommands.Prefix,
+			Wok.DefaultCommands.RequiredPermissions,
+			Wok.DefaultCommands.RequiredRoles,
+			Wok.DefaultCommands.ToggleCommand
+		],
 
-	wok.on("databaseConnected", async (connection, state) => {
-		console.log("WOKCommands > Database", state)
-	})
+		mongoUri: process.env.DB,
+	});
 }); client.on('levelUp', async (message, data, role) => { console.log(data) }) // <= This is optional but can be used to send a message to the user when they level up.
 
 client.login(process.env.TOKEN) // you'll need to set this to your own Discord Bot Token
